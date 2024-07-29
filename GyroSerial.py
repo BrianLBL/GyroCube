ee7d85fd014125cbf486d63bf2512d3293adae93
Brian Luu <120427730+BrianLBL@users.noreply.github.com>
2024-07-29 08:09:24 -0400

    Arduino Upload Code and Updated Python Serial File

--- GyroSerial.py
+++ GyroSerial.py
@@ -1,14 +1,32 @@
 #Reads serial data from gyroscope sensor
 import serial
+import serial.tools.list_ports
 import time
 
-#Initialize (Reset module and )
+#Initialize (Starts the transmission)
+ports = serial.tools.list_ports.comports();
+ser = serial.Serial()
+portList = []
+for comPort in ports:
+    portList.append(str(comPort))
+    print(str(comPort))
+    
+val = input("select Port: COM")
+
+for x in range(0,len(portList)):
+    if portList[x].startswith("COM" + str(val)):
+        portVar = "COM" + str(val)
+        print(portList[x])
+        
+print(val)
+        
 MPU = int("68",16)
-ser = serial.Serial('COM3', 19200, timeout = 1)   #Please set correct port value if not COM3
-print(ser.name) #Testing which port was open
-ser.write(int("6B"),16) #Communicate with register 6B
-ser.write(int("00"),16) #Resets by writing 0 into register 6B
-ser.close() #Ends the transmission
+ser.baudrate = 19200 #Set the same baudrate as arduino code on module
+ser.port = portVar
+ser.open()
 
 #Loop (Continuously Reads Data)
-
+while True:
+    if ser.is_waiting:
+        packet = ser.readline()
+        print(packet.decode('utf'))
